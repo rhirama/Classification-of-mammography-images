1c6b0568870a4b92ed9afd4b7dad0fb6df547846
Rodrigo Hirama <rodrigo.hirama@usp.br>
2018-11-07 07:51:13 -0200

    mac - divisão entre arquivos Process.py e Spiculation.py

--- Python/Spiculation.py
+++ Python/Spiculation.py
@@ -1,19 +1,6 @@
 import cv2
 import numpy as np
 import math
-import importlib
-
-imgLoader = importlib.import_module('ImgLoader')
-
-def maxAreaContour(contours):
-    cnt = contours[0]
-    max_area = cv2.contourArea(cnt)
-
-    for cont in contours:
-        if cv2.contourArea(cont) > max_area:
-            cnt = cont
-            max_area = cv2.contourArea(cont)
-    return cnt
 
 def angle(x1, y1, x2, y2):  # calcula o angulo entre dois vetores usando o produto escalar
     dist1 = math.sqrt(x1**2 + y1**2)
@@ -43,16 +30,3 @@ def calcultateSpiculationIndex(contour):
 
     spiculationIndex = numer/denom
     return spiculationIndex
-
-name = 'SPIC_DB_207.jpg'
-
-imgColor, imgGray = imgLoader.loadImg(name)
-canvas = imgLoader.createClearCanvas(imgColor)
-contours = imgLoader.preProcess(imgGray)
-cnt = maxAreaContour(contours)
-
-perimeter = int(cv2.arcLength(cnt, False))
-epsilon = 0.01*perimeter
-approx = cv2.approxPolyDP(cnt, epsilon, False)# parâmetros para testar: epsilon(dita o quao simplificada fica a figura)
-si = calcultateSpiculationIndex(approx)
-print(si)
