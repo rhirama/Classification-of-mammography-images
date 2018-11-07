1b59dd8d37ed00fccc0abc8396ebf4b0e2ed9e78
Rodrigo Hirama <rhirama@users.noreply.github.com>
2018-11-07 00:28:35 -0200

    windows - cáluclo do spiculation index terminado

--- Python/Spiculation.py
+++ Python/Spiculation.py
@@ -5,17 +5,19 @@ import importlib
 
 imgLoader = importlib.import_module('ImgLoader')
 
+
 def maxAreaContour(contours):
-	cnt = contours[0]
-	max_area = cv2.contourArea(cnt)
+    cnt = contours[0]
+    max_area = cv2.contourArea(cnt)
 
-	for cont in contours:
-		if cv2.contourArea(cont) > max_area:
-			cnt = cont
-			max_area = cv2.contourArea(cont)
-	return cnt
+    for cont in contours:
+        if cv2.contourArea(cont) > max_area:
+            cnt = cont
+            max_area = cv2.contourArea(cont)
+    return cnt
 
-def angle(x1, y1, x2, y2): #calcula o angulo entre dois vetores usando o produto escalar
+
+def angle(x1, y1, x2, y2):  # calcula o angulo entre dois vetores usando o produto escalar
     dist1 = math.sqrt(x1**2 + y1**2)
     dist2 = math.sqrt(x2**2 + y2**2)
 
@@ -23,8 +25,36 @@ def angle(x1, y1, x2, y2): #calcula o angulo entre dois vetores usando o produto
     denom = dist1 * dist2
     return math.degrees(math.acos(numer/denom)), dist2
 
-def crossSign(x1, y1, x2, y2): #checa se o angulo eh externo ou interno, neste caso, será true quando for interno e false quando for externo
+
+def crossSign(x1, y1, x2, y2):  # checa se o angulo eh externo ou interno, neste caso, será true quando for interno e false quando for externo
     return x1 * y2 > x2 * y1
+
+
+def calcultateSpiculationIndex(contour):
+    numer = 0
+    denom = 0
+
+    for i in range(len(approx)):
+        line2 = approx[i][0]
+        ref = approx[i-1][0]
+        line1 = approx[i-2][0]
+        x1, y1 = line1[0] - ref[0], line1[1] - ref[1]
+        x2, y2 = line2[0] - ref[0], line2[1] - ref[1]
+
+        if crossSign(x1, y1, x2, y2):
+            ang, vLength = angle(x1, y1, x2, y2)
+        else:
+            ang, vLength = angle(x1, y1, x2, y2)
+            ang = 360 - ang
+
+        print(ang)
+
+        numer += (1 + math.cos(math.radians(ang))) * vLength
+        denom += vLength
+
+    spiculationIndex = numer/denom
+    return spiculationIndex
+
 
 name = 'SPIC_DB_207.jpg'
 
@@ -35,27 +65,5 @@ cnt = maxAreaContour(contours)
 
 perimeter = int(cv2.arcLength(cnt, False))
 epsilon = 0.01*perimeter
-approx = cv2.approxPolyDP(cnt, epsilon, False) #parâmetros para testar: epsilon(dita o quao simplificada fica a figura)
-
-numer = 0
-denom = 0
-
-for i in range(len(approx)):
-    line2 = approx[i]
-    ref = approx[i-1]
-    line1 = approx[i-2]
-    x1, y1 = line1[0] - ref[0], line1[1] - ref[1]
-    x2, y2 = line2[0] - ref[0], line2[1] - ref[1]
-
-    if crossSign(x1, y1, x2, y2):
-        ang, vLength = angle(x1, y1, x2, y2)
-    else: 
-        ang, vLength = angle(x1, y1, x2, y2)
-        ang = 360 - ang
-
-    numer += (1 + math.cos(math.radians(ang))) * vLength
-    denom += vLength
-
-spiculationIndex = numer/denom
-
-    
+# parâmetros para testar: epsilon(dita o quao simplificada fica a figura)
+approx = cv2.approxPolyDP(cnt, epsilon, False)
