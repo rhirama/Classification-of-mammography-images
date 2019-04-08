8b4515394a04c0936fb126b4ca8f09a9ef9b4ebf
Rodrigo Hirama <rodrigo.hirama@usp.br>
2019-04-08 13:06:06 -0300

    mac - implementando alterações si

--- Python/Spiculation.py
+++ Python/Spiculation.py
@@ -2,30 +2,42 @@ import numpy as np
 import math
 
 
-def angle(x1, y1, x2, y2):  # calcula o angulo entre dois vetores usando o produto escalar
+def angle(p1, p2, p3):  # calcula o angulo entre dois vetores usando o produto escalar
+
+    x1, y1 = p1[0] - p2[0], p1[1] - p2[1]
+    x2, y2 = p3[0] - p2[0], p3[1] - p2[1]
+
     dist1 = math.sqrt(x1**2 + y1**2)
     dist2 = math.sqrt(x2**2 + y2**2)
 
     numer = (x1 * x2 + y1 * y2)
     denom = dist1 * dist2
-    return math.degrees(math.acos(numer/denom)), dist2
+    ang_aux = math.degrees(math.acos(numer/denom))
+    
+    z_direction = ((p2[0] - p1[0])*(p3[1] - p2[1])) - ((p3[0] - p2[0])*(p2[1] - p1[1]))
 
+    if z_direction > 0:
+        ang = 180 - ang_aux
+    
+    elif z_direction < 0:
+        ang = 180 + ang_aux
+
+    return ang, dist2
 
 def calculate_spiculation_index(approx):
     numer = 0
     denom = 0
 
     for i in range(len(approx)):
-        line2 = approx[i][0]
-        ref = approx[i-1][0]
+
         line1 = approx[i-2][0]
-        x1, y1 = line1[0] - ref[0], line1[1] - ref[1]
-        x2, y2 = line2[0] - ref[0], line2[1] - ref[1]
+        ref = approx[i-1][0]
+        line2 = approx[i][0] 
 
-        ang, v_length = angle(x1, y1, x2, y2)
+        ang, v_length = angle(line1, ref, line2)
 
-        #print(ang)
-
+        # print(ang)
+        print(ang)
         numer += (1 + math.cos(math.radians(ang))) * v_length
         denom += v_length
 
